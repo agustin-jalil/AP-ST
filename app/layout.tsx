d5118a6e12d9de12eb49093db3045cc6199700ec
agustin <agustinjalilleon@gmail.com>
2026-03-02 10:02:02 -0300

    primer commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'TodoApple',
+  description: 'TodoApple',
+  generator: 'TodoApple',
 }
 
 export default function RootLayout({
